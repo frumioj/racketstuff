4523ef93cea1c2426c7de7e3f5b1cceaef1c94e8
John Kemp <stable.pseudonym@gmail.com>
2022-01-21 15:50:45 -0500

    add string concatenation exercise

--- distance.rkt
+++ distance.rkt
@@ -1,2 +1,8 @@
 (define (cart_distance x y)
   (sqrt (+ (* x x) (* y y))))
+
+(define (concat prefix suffix)
+  (cond
+    [(eq? prefix "") suffix]
+    [(eq? suffix "") prefix]
+    [else (string-append prefix "_" suffix)]))
